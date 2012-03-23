3e7aad7d61d5e7a1875063a1d34472ed5de42571
Stephen LaPorte <stephen.laporte@gmail.com>
2012-03-23 11:13:46 -0700

    limit to 25

--- js/pin.js
+++ js/pin.js
@@ -1,5 +1,5 @@
 function loadPinboard(div) {
-	$.getJSON('http://feeds.pinboard.in/json/v1/u:slaporte/?count=15&cb=?', function(data) {
+	$.getJSON('http://feeds.pinboard.in/json/v1/u:slaporte/?count=25&cb=?', function(data) {
 		for (var i = 0; i < data.length; i++) {
 			var url = data[i].u;
 			var title = data[i].d;
